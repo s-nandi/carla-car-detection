ac39a1ac77daaad69e94aa6ba364955a8bd5dba1
s-nandi <saujas.nandi@gmail.com>
2019-10-23 02:44:49 -0400

    Add pull scripts

---
+++ evaluations/pull_evals.sh
@@ -0,0 +1,1 @@
+scp -r saujas@sunlight.cs.unc.edu:/home/saujas/carla-car-detection/models/research/object_detection/Tensorflow-Carla-Object-Detection/evaluation/summaries/* full_evaluation/
---
+++ full_trained_model/pull_checkpoints.sh
@@ -0,0 +1,1 @@
+rsync -avh -e ssh saujas@sunlight.cs.unc.edu:/home/saujas/carla-car-detection/models/research/object_detection/Tensorflow-Carla-Object-Detection/training/output/* checkpoints
